97a6a69c7d03aede00b71811beacbdfe568c06d5
xiehaitao0229 <229760995@qq.com>
2019-08-05 15:52:14 +0800

    完成回文土木

--- code/string/05.js
+++ code/string/05.js
@@ -7,7 +7,7 @@ var longestCommonPrefix = function (strs) {
     let one = strs.length > 0 ? String(strs[0]).split("") : false;  //  拿出数组中其中一个就可以
     let str = "";
     if (!one) { return str; };
-    for (let i = 0; i < one.length; i++) {
+    for (let i = 0; i < one.length;  i++) {
         let num = 0;
         strs.map(item => {  //  便利每个单词是否和第一个单词有最长公共前缀
             item.charAt(i) == one[i] ? num++ : null
---
+++ code/string/07.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(nums) {
+    let arr = String(nums).split('');
+    return arr.join('')===arr.reverse().join('');
+};
+
+console.log(isPalindrome(-121))
